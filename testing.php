ffc5ad56994c93b992bb36a64633b5e106cc35ec
gdochadipa <gdochadipa@gmail.com>
2019-06-26 20:26:36 +0800

    testi2

--- testing.php
+++ testing.php
@@ -1,3 +1,21 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "./random_string.php";
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=myexampleapp;AccountKey=zeniCmdQxnNBMn+GJJhlZLgUxFrl3hLnisNngUuj9s+b19UG7F5L/hjy2zWp4k1oaeJm9tklKqA5YnHiBl3RAw==";
+
+// Create blob client.
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+
+
+ ?>
+
 <html>
  <head>
  <Title>Registration Form</Title>
@@ -38,17 +56,77 @@
     }
 
     if (isset($_POST['submit'])) {
-        try {
-            $gambar = $_POST['gambar'];
-            // Insert data
-            $sql_insert = "INSERT INTO tbl_vision2 (gambar)
-                        VALUES (?)";
-            $stmt = $conn->prepare($sql_insert);
-            $stmt->bindValue(1, $gambar);
+      $createContainerOptions = new CreateContainerOptions();
+      $createContainerOptions->addMetaData("key1", "value1");
+      $createContainerOptions->addMetaData("key2", "value2");
 
-            $stmt->execute();
+        $containerName = "blockblobs".generateRandomString();
+
+        try {
+          $gambar = $_FILES['gambar']['name'];
+          $sizeFile = $_FILES['gambar']['size'];
+          $typeFile = $_FILES['gambar']['type'];
+          $fileToUpload = $_FILES['gambar']['tmp_name'];
+
+          // Create container.
+          $blobClient->createContainer($containerName, $createContainerOptions);
+
+          // Getting local file so that we can upload it to Azure
+          $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
+          fclose($myfile);
+
+          $content = fopen($fileToUpload, "r");
+
+          //Upload blob
+          $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+
+          // List blobs.
+          $listBlobsOptions = new ListBlobsOptions();
+          $listBlobsOptions->setPrefix("HelloWorld");
+
+          do{
+              $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+              foreach ($result->getBlobs() as $blob)
+              {
+                  echo $blob->getName().": ".$blob->getUrl()."<br />";
+              }
+
+              $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+          } while($result->getContinuationToken());
+          echo "<br />";
+
+          // Get blob.
+          echo "This is the content of the blob uploaded: ";
+          $blob = $blobClient->getBlob($containerName, $fileToUpload);
+          fpassthru($blob->getContentStream());
+          echo "<br />";
+          //  $gambar = $_POST['gambar'];
+            // Insert data
+          //  $sql_insert = "INSERT INTO tbl_vision2 (gambar)
+          //              VALUES (?)";
+          //  $stmt = $conn->prepare($sql_insert);
+          //  $stmt->bindValue(1, $gambar);
+
+          //  $stmt->execute();
         } catch(Exception $e) {
             echo "Failed: " . $e;
+        }
+
+        catch(ServiceException $e){
+            // Handle exception based on error codes and messages.
+            // Error codes and messages are here:
+            // http://msdn.microsoft.com/library/azure/dd179439.aspx
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code.": ".$error_message."<br />";
+        }
+        catch(InvalidArgumentTypeException $e){
+            // Handle exception based on error codes and messages.
+            // Error codes and messages are here:
+            // http://msdn.microsoft.com/library/azure/dd179439.aspx
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code.": ".$error_message."<br />";
         }
 
         echo "<h3>Your're registered!</h3>";
@@ -60,10 +138,8 @@
             if(count($registrants) > 0) {
                 echo "<h2>People who are registered:</h2>";
                 echo "<table>";
-                echo "<tr><th>Name</th>";
-                echo "<th>Email</th>";
-                echo "<th>Job</th>";
-                echo "<th>Date</th></tr>";
+                echo "<tr><th>Gambar</th>";
+                echo "<th>Action</th></tr>";
                 foreach($registrants as $registrant) {
                     echo "<tr><td>".$registrant['gambar']."</td>";
                     echo "<td><a href=''>Analisa</a>  </td></tr>";
