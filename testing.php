814d439e329e8a28779ba708c3b89ef62dae3cee
gdochadipa <gdochadipa@gmail.com>
2019-06-26 20:06:30 +0800

    update

--- testing.php
+++ testing.php
@@ -18,10 +18,9 @@
  <body>
  <h1>Register here!</h1>
  <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br></br>
-       Email <input type="text" name="email" id="email"/></br></br>
-       Job <input type="text" name="job" id="job"/></br></br>
+ <form method="post" action="testing.php" enctype="multipart/form-data" >
+       Gambar  <input type="file" name="name" id="name"/></br></br>
+
        <input type="submit" name="submit" value="Submit" />
        <input type="submit" name="load_data" value="Load Data" />
  </form>
@@ -40,18 +39,13 @@
 
     if (isset($_POST['submit'])) {
         try {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $job = $_POST['job'];
-            $date = date("Y-m-d");
+            $gambar = $_POST['gambar'];
             // Insert data
-            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
-                        VALUES (?,?,?,?)";
+            $sql_insert = "INSERT INTO tbl_vision2 (gambar)
+                        VALUES (?)";
             $stmt = $conn->prepare($sql_insert);
-            $stmt->bindValue(1, $name);
-            $stmt->bindValue(2, $email);
-            $stmt->bindValue(3, $job);
-            $stmt->bindValue(4, $date);
+            $stmt->bindValue(1, $gambar);
+
             $stmt->execute();
         } catch(Exception $e) {
             echo "Failed: " . $e;
@@ -60,9 +54,9 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM Registration";
+            $sql_select = "SELECT * FROM tbl_vision2";
             $stmt = $conn->query($sql_select);
-            $registrants = $stmt->fetchAll(); 
+            $registrants = $stmt->fetchAll();
             if(count($registrants) > 0) {
                 echo "<h2>People who are registered:</h2>";
                 echo "<table>";
@@ -71,10 +65,8 @@
                 echo "<th>Job</th>";
                 echo "<th>Date</th></tr>";
                 foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['name']."</td>";
-                    echo "<td>".$registrant['email']."</td>";
-                    echo "<td>".$registrant['job']."</td>";
-                    echo "<td>".$registrant['date']."</td></tr>";
+                    echo "<tr><td>".$registrant['gambar']."</td>";
+                    echo "<td><a href=''>Analisa</a>  </td></tr>";
                 }
                 echo "</table>";
             } else {
@@ -85,5 +77,6 @@
         }
     }
  ?>
+
  </body>
  </html>
