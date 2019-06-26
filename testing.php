9ea2f2e3857587e0d485c7a3f5e50a9103dca42d
gdochadipa <gdochadipa@gmail.com>
2019-06-26 21:12:52 +0800

    testi2

--- testing.php
+++ testing.php
@@ -72,13 +72,13 @@ $blobClient = BlobRestProxy::createBlobService($connectionString);
           $blobClient->createContainer($containerName, $createContainerOptions);
 
           // Getting local file so that we can upload it to Azure
-          $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
+          $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
           fclose($myfile);
 
           $content = fopen($fileToUpload, "r");
 
           //Upload blob
-          $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+          $blobClient->createBlockBlob($containerName, $gambar, $content);
 
           // List blobs.
           $listBlobsOptions = new ListBlobsOptions();
